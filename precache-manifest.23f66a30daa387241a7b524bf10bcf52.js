05c90f6a4a11d9136ec03065c2e9157a364dda7e
Armando <jaimes087.uz@gmail.com>
2020-10-03 11:17:27 -0500

    Updates

---
+++ precache-manifest.23f66a30daa387241a7b524bf10bcf52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ebc376452125f3f1e483cb8f0b8f5f",
+    "url": "/react-login-ui/index.html"
+  },
+  {
+    "revision": "4552338e1f62e3558e4c",
+    "url": "/react-login-ui/static/css/main.fc6cf6a6.chunk.css"
+  },
+  {
+    "revision": "ed4ea3fa514531626558",
+    "url": "/react-login-ui/static/js/2.5285333d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-login-ui/static/js/2.5285333d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4552338e1f62e3558e4c",
+    "url": "/react-login-ui/static/js/main.e14d6c5f.chunk.js"
+  },
+  {
+    "revision": "4535eaf6fdd31e7e3d3a",
+    "url": "/react-login-ui/static/js/runtime-main.14943fcc.js"
+  },
+  {
+    "revision": "2209dcb8d07f8b7480170642b1fab99b",
+    "url": "/react-login-ui/static/media/cover.2209dcb8.jpg"
+  },
+  {
+    "revision": "788bf9c28c0b5e54f2bdc4f831c56dd1",
+    "url": "/react-login-ui/static/media/material-error-outline.788bf9c2.svg"
+  }
+]);
